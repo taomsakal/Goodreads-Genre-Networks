d27e540c2a8a286d4ff2c6c7f49452114a6ad878
Taom Sakal <taom.sakal@lifesci.ucsb.edu>
2017-02-26 16:05:12 -0800

    Crawled some users
    Former-commit-id: 906c20013c32dcf762f3b1df60efe3fecc7e01ef

--- crawler/extracted_data/investigations.py
+++ crawler/extracted_data/investigations.py
@@ -5,6 +5,6 @@ import pickle
 
 data = pickle.load(open("userlist_0_data", "rb"))
 
-print(data[1].dataframe)
+print(data[1001].dataframe)
 print("--------")
 print("Number of users: " + str(len(data)))
