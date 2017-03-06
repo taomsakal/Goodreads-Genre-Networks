bfe454532a5ee628d54a2de7d1e6fbaa4bf1a0fa
Taom Sakal <taom.sakal@lifesci.ucsb.edu>
2017-03-05 21:17:14 -0800

    Now chunks data every 1000 users so not dealing with too big files.
    Former-commit-id: 5882d0c47b304bb594ade77e95cbbc7dfb998f8d

--- crawler/extracted_data/investigations.py
+++ crawler/extracted_data/investigations.py
@@ -1,9 +1,16 @@
 "Scratch file to look at extracted data"
 
-from crawler.general import read
+from crawler.general import read, overwrite
 
-data = read("userlist_0_data")
+data = read("userlist_0_data_1")
 
 print(data[-1].dataframe)
 print("--------")
 print("Number of users: " + str(len(data)))
+print(len(data[-1].dataframe))
+
+data1 = data[0:5000]
+data2 = data[5000:10000]
+
+overwrite(data1, "userlist_0_data_1_1")
+overwrite(data2, "userlist_0_data_1_2")
